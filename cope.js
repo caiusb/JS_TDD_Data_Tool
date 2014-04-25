65878ef334adfbe5a60968494a75df7cf8398536
Michael Hilton <michael.hilton@gmail.com>
2014-04-25 16:20:49 -0700

    first commit of JS tool for marking up JSON

---
+++ cope.js
@@ -0,0 +1,13 @@
+<script>
+
+$.getJSON( "intermediateJSON.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    console.log(val);
+  });
+ 
+ 
+});
+
+
+<\script>
