f1beac28ae05256327330a559e54740b952bc955
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-05-16 14:14:13 -0700

    Drawing one line!!!

--- radarChart.js
+++ radarChart.js
@@ -60,9 +60,10 @@ var RadarChart = {
 			.append("g")
 			.attr("class", "axis");
 
+  // the "axis" lines
 	axis.append("line")
 		.attr("x1", cfg.w/2)
-		.attr("y1", cfg.h/2)
+		.attr("y1", cfg.h/2) // all the lines meet in the midle of the chart
 		.attr("x2", function(d, i){return cfg.w/2*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
 		.attr("y2", function(d, i){return cfg.h/2*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
 		.attr("class", "line")
@@ -80,6 +81,19 @@ var RadarChart = {
 		.attr("x", function(d, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-60*Math.sin(i*cfg.radians/total);})
 		.attr("y", function(d, i){return cfg.h/2*(1-Math.cos(i*cfg.radians/total))-20*Math.cos(i*cfg.radians/total);});
 
+  var dd = d[0];
+
+  g.selectAll(".area")
+    .data(dd)
+    .enter()
+    .append("line")
+    .attr("x1", cfg.w/2 * (1-(parseFloat(Math.max(dd[0].value, 0)/cfg.maxValue)*cfg.factor*Math.sin(0*cfg.radians/total))))
+    .attr("y1", cfg.h/2 * (1-(parseFloat(Math.max(dd[0].value, 0)/cfg.maxValue)*cfg.factor*Math.cos(0*cfg.radians/total))))
+    .attr("x2", cfg.w/2 * (1-(parseFloat(Math.max(dd[1].value, 0)/cfg.maxValue)*cfg.factor*Math.sin(1*cfg.radians/total))))
+    .attr("y2", cfg.h/2 * (1-(parseFloat(Math.max(dd[1].value, 0)/cfg.maxValue)*cfg.factor*Math.cos(1*cfg.radians/total))))
+    .style("stroke-width", "2px")
+    .style("stroke", cfg.color(series));
+
 
 	d.forEach(function(y, x){
 	  dataValues = [];
@@ -91,36 +105,39 @@ var RadarChart = {
 		  ]);
 		});
 	  dataValues.push(dataValues[0]);
-	  g.selectAll(".area")
-					 .data([dataValues])
-					 .enter()
-					 .append("polygon")
-					 .attr("class", "radar-chart-serie"+series)
-					 .style("stroke-width", "2px")
-					 .style("stroke", cfg.color(series))
-					 .attr("points",function(d) {
-						 var str="";
-						 for(var pti=0;pti<d.length;pti++){
-							 str=str+d[pti][0]+","+d[pti][1]+" ";
-						 }
-						 return str;
-					  })
-					 .style("fill", function(j, i){return cfg.color(series)})
-					 .style("fill-opacity", cfg.opacityArea)
-					 .on('mouseover', function (d){
-										z = "polygon."+d3.select(this).attr("class");
-										g.selectAll("polygon")
-										 .transition(200)
-										 .style("fill-opacity", 0.1);
-										g.selectAll(z)
-										 .transition(200)
-										 .style("fill-opacity", .7);
-									  })
-					 .on('mouseout', function(){
-										g.selectAll("polygon")
-										 .transition(200)
-										 .style("fill-opacity", cfg.opacityArea);
-					 });
+	  // g.selectAll(".area")
+		// 			 .data(dataValues)
+		// 			 .enter()
+		// 			 //.append("polygon")
+    //        .append("line")
+		// 			 .attr("class", "radar-chart-serie"+series)
+		// 			 .style("stroke-width", "2px")
+		// 			 .style("stroke", cfg.color(series))
+    //        .attr("x");
+					//  .attr("points",function(d) {
+					// 	 var str="";
+					// 	 for(var pti=0;pti<d.length;pti++){
+					// 		 str=str+d[pti][0]+","+d[pti][1]+" ";
+					// 	 }
+					// 	 return str;
+					//   })
+					//  .style("fill", function(j, i){return cfg.color(series)})
+					//  .style("fill-opacity", cfg.opacityArea)
+					//  .on('mouseover', function (d){
+					// 					z = "polygon."+d3.select(this).attr("class");
+					// 					g.selectAll("polygon")
+					// 					 .transition(200)
+					// 					 .style("fill-opacity", 0.1);
+					// 					g.selectAll(z)
+					// 					 .transition(200)
+					// 					 .style("fill-opacity", .7);
+					// 				  })
+					//  .on('mouseout', function(){
+					// 					g.selectAll("polygon")
+					// 					 .transition(200)
+					// 					 .style("fill-opacity", cfg.opacityArea);
+					//  });
+    console.log(dataValues)
 	  series++;
 	});
 	series=0;
